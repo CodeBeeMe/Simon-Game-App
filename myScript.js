36a75451b5621e008b7fdada79fd48d34360363d
Cătălin Anghel-Ursu <catalee.xen@gmail.com>
2018-01-09 18:05:30 +0200

    Update myScript.js
    Added a userInput() function to be used with the onclick event added to the 4 colored buttons. Testing this method of input and interaction with the game logic.

--- myScript.js
+++ myScript.js
@@ -29,84 +29,116 @@ $(document).ready(function() {
       uiOuter = $(".l-circle"),
       uiSwitch = $(".switch"),
       uiStrict = $(".strict"),
-      uiPlay = $(".play, i"),
-      uiStart = $(".start"),
+      uiFooter = $(".footer"),
+      uiPlayBox = $(".play-box"),
+      uiPlay = $(".play"),
+      uiStart = $(".start"),      
+      uiLevel = $(".level"),
       uiOnOff = $(".knob"),
       uiGreen = $(".green"),
       uiYlow = $(".yellow"),
       uiBlue = $(".blue"),
       uiRed = $(".red"),
-      strict = false;
-  
-  //function for checking if the strict mode is on or off  
-  uiStrict.click(function isStrict() {
-      if (!strict) {
-        uiStrict.addClass("str-on");
-        strict = true;
-      } else {
-        uiStrict.removeClass("str-on");
-        strict = false;
-      }
-    });
-  
-  function toggleOffToOn() {
-    uiSwitch.css("background", "#91e842");
-    uiOnOff.removeClass("slide-l");
-    uiOnOff.addClass("slide-r");
-    uiDisplay.removeClass("invisible");        
-    $(this).one("click", toggleOnToOff);
-  }
-  function toggleOnToOff() {
-    uiSwitch.css("background", "#ff725e");
-    uiOnOff.removeClass("slide-r");
-    uiOnOff.addClass("slide-l");
-    uiDisplay.addClass("invisible");
-    uiStrict.removeClass("str-on");
-    strict = false;
-    $(this).one("click", toggleOffToOn);
-  }
-  uiSwitch.one("click", toggleOffToOn);
+      strict = false,
+      on = false,
+      level = 1;
+
+  level = level < 10 ? "0" + level : level;
 
   uiPlay.addClass("color-me"); //add a 4 color animation on the play button
 
   //starts the game wich in turn starts a couple of animations
   uiPlay.click(function() {
     uiPlay.css("transform", "scale(1.2, 1.2)");
-    uiPlay.css("opacity", ".5");
+    uiPlay.css("opacity", ".5");    
 
     setTimeout(function() {
-      uiPlay.css("display", "none");
-      uiOuter.css("display", "block");
+      uiPlayBox.css("display", "none");
+      uiOuter.css("display", "block");      
     }, 100);
 
     setTimeout(function() {
       uiGreen.addClass("visible");
-      uiGreen.removeClass("invisible");
     }, 500);
 
     setTimeout(function() {
       uiRed.addClass("visible");
-      uiRed.removeClass("invisible");
     }, 600);
 
     setTimeout(function() {
       uiYlow.addClass("visible");
-      uiYlow.removeClass("invisible");
     }, 700);
 
     setTimeout(function() {
       uiBlue.addClass("visible");
-      uiBlue.removeClass("invisible");
     }, 800);
 
     setTimeout(function() {
       uiCenter.addClass("zoom-in");
       uiCenter.removeClass("invisible");
       uiOuter.removeClass("invisible");
+      uiFooter.css("display", "block");
+      uiFooter.addClass("visible");
     }, 1000);
 
     setTimeout(function() {
       uiOuter.css("box-shadow", "inset 0px 0px 50px -20px rgba(0, 0, 0, 1)");
     }, 1500);
   });
+
+  //function for checking if the strict mode is on or off
+  uiStrict.click(function isStrict() {
+    if (strict) {
+      uiStrict.addClass("str-on");
+      strict = false;
+    } else {
+      uiStrict.removeClass("str-on");
+      strict = true;
+    }
+  });
+  
+  function toggleOffToOn() {
+    on = true;
+    strict = true;
+    uiLevel.html("..");
+    pushedPads = [];
+    uiLevel.removeClass("zoom-in");
+    uiSwitch.css("background", "#91e842");
+    uiOnOff.removeClass("slide-l");
+    uiOnOff.addClass("slide-r");
+    uiDisplay.removeClass("invisible");
+    if (on) {
+      uiStart.click(function() {
+        //level = 1;
+        uiLevel.addClass("zoom-in");
+        uiLevel.html(level);
+      });
+    }
+    $(this).one("click", toggleOnToOff);    
+  }
+  
+  function toggleOnToOff() {
+    on = false;
+    strict = false;
+    pushedPads = [];
+    uiLevel.html("..");
+    uiLevel.removeClass("zoom-in");
+    uiSwitch.css("background", "#f44242");
+    uiOnOff.removeClass("slide-r");
+    uiOnOff.addClass("slide-l");
+    uiDisplay.addClass("invisible");
+    uiStrict.removeClass("str-on");
+    $(this).one("click", toggleOffToOn);
+  }
+  uiSwitch.one("click", toggleOffToOn);
+
+  
 });
+
+var pushedPads = [];
+function userInput(el) {
+  pushedPads.push(el);
+  console.log(pushedPads);
+}
+
+
